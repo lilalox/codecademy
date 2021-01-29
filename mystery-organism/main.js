fd2bb6f9eae4a3fc7a0f47c6db5880e370c85182
lilalox <learouiller@gmail.com>
2021-01-29 11:35:09 +0100

    Added extra challenges / added comments

--- mystery-organism/main.js
+++ mystery-organism/main.js
@@ -1,3 +1,4 @@
+// Returns a random index based on an array's length
 const randIndex = (arr) => Math.floor(Math.random() * arr.length);
 
 // Returns a random DNA base
@@ -15,6 +16,7 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// Makes P-Aequor objects
 const pAequorFactory = (specimenNum, dna) => {
   return {
     specimenNum,
@@ -33,18 +35,19 @@ const pAequorFactory = (specimenNum, dna) => {
     },
     compareDNA(otherSpecimen) {
       let sameDNA = 0;
-      for (i = 0; i < this.dna.length; i++) {
+      for (let i = 0; i < this.dna.length; i++) {
         if (this.dna[i] === otherSpecimen.dna[i]) {
           sameDNA++
         }
       }
       const dnaPercent = Math.ceil(sameDNA / this.dna.length * 100)
-      console.log(`Specimen #${this.specimenNum} and #${otherSpecimen.specimenNum} have ${dnaPercent}% DNA in common.`)
+      //console.log(`Specimen #${this.specimenNum} and #${otherSpecimen.specimenNum} have ${dnaPercent}% DNA in common.`)
+      return dnaPercent;
     },
     willLikelySurvive() {
       const goodBases = ['C', 'G'];
       let nrOfGoodBases = 0;
-      for (i = 0; i < this.dna.length; i++) {
+      for (let i = 0; i < this.dna.length; i++) {
         if (goodBases.includes(this.dna[i])) {
           nrOfGoodBases++
         }
@@ -55,24 +58,59 @@ const pAequorFactory = (specimenNum, dna) => {
       } else {
         return false
       }
+    },
+    complementStrand() {
+      const complBases = [['A', 'T'], ['T', 'A'], ['C', 'G'], ['G', 'C']];
+      const complDNA = [];
+      for (let i = 0; i < this.dna.length; i++) {
+        for (let j = 0; j < complBases.length; j++) {
+          if (this.dna[i] === complBases[j][0]) {
+            complDNA.push(complBases[j][1])
+          }
+        }
+      }
+      return complDNA;
     }
   }
 }
 
+// Finds a defined number of viable P-Aequors based on DNA characteristics
 const makeViablePAequors = (num) => {
   const viablePAequors = [];
+  const output = [];
   let i = 0;
   while (viablePAequors.length < num) {
     const newSpecimen = pAequorFactory(i, mockUpStrand());
     if (newSpecimen.willLikelySurvive()) {
       viablePAequors.push(newSpecimen)
-      console.log(`Specimen #${newSpecimen.specimenNum} was added to the array. ${viablePAequors.length}/30`)
+      //console.log(`Specimen #${newSpecimen.specimenNum} was added to the array. ${viablePAequors.length}/${num}`)
+      output.push('#' + newSpecimen.specimenNum)
     }
     i++;
   }
+  console.log(`Viable P-Aequors (${num} total): ` + output.join(', '))
   return viablePAequors;
 }
 
-console.log(makeViablePAequors(30));
+//Finds the two most related (DNA-wise) specimen in an array
+const findMostRelated = (arr) => {
+  const mostRelated = { specimen1: null, specimen2: null, percentage: 0 };
+  for (let i = 0; i < arr.length; i++) {
+    const compareArray = arr.slice()
+    compareArray.splice(i, 1)
+    for (let j = 0; j < compareArray.length; j++) {
+      const percentage = arr[i].compareDNA(compareArray[j]);
+      if (percentage > mostRelated.percentage) {
+        mostRelated.specimen1 = arr[i]
+        mostRelated.specimen2 = compareArray[j]
+        mostRelated.percentage = percentage
+      }
+    }
+  }
+  console.log(`The most related specimen among the ${arr.length} analysed are #${mostRelated.specimen1.specimenNum} amd #${mostRelated.specimen2.specimenNum}, with ${mostRelated.percentage}% common DNA.`)
+  return mostRelated;
+}
 
+const viable = makeViablePAequors(30);
+findMostRelated(viable);
 
